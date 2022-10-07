c973890e06454968cf4e8e52b3a7c2f6d827c5f2
seunga2590 <74227600+seunga2590@users.noreply.github.com>
2022-10-07 11:24:11 +0900

    [level 1] Title: 음양 더하기, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = []
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer.append(absolutes[i])
+        else:
+            answer.append(-absolutes[i])
+            
+    return sum(answer)
